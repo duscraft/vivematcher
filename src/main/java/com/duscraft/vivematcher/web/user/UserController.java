676e33ec7930f88643df444910f6cb5ebd91c766
Antoine Laborderie <antoine.laborderie@gmail.com>
2025-10-24 22:33:12 +0200

    feat: throw proper statuses and return created status on user creation

--- src/main/java/com/duscraft/vivematcher/web/user/UserController.java
+++ src/main/java/com/duscraft/vivematcher/web/user/UserController.java
@@ -3,6 +3,8 @@ package com.duscraft.vivematcher.web.user;
 import com.duscraft.vivematcher.domain.user.User;
 import com.duscraft.vivematcher.service.user.UserService;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+import org.springframework.http.HttpStatus;
 
 @RestController
 public class UserController {
@@ -13,6 +15,7 @@ public class UserController {
   }
 
   @PostMapping("/users")
+  @ResponseStatus(HttpStatus.CREATED)
   public User createUser(@RequestBody CreateUserRequest request) {
     return this.userService.createUser(
         request.getDisplayName(),
@@ -22,11 +25,13 @@ public class UserController {
 
   @GetMapping("/users/{id}")
   public User getUser(@PathVariable Long id) {
-    return this.userService.getById(id).orElseThrow();
+    return this.userService.getById(id)
+        .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "User not found"));
   }
 
   @GetMapping("/users/by-name/{name}")
   public User findUserByName(@PathVariable String name) {
-    return this.userService.findByDisplayName(name).orElseThrow();
+    return this.userService.findByDisplayName(name)
+        .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "User not found"));
   }
 }
